0c0e5da587b54426eb52bfad1d7f48a16128c0f9
Nikita Ketov <nketov@bigmir.net>
2018-10-03 17:12:35 +0300

    03102018

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -129,8 +129,8 @@ class SiteController extends Controller
     {
         $model = new ContactForm();
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
-                Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
+            if ($model->sendEmail()) {
+                Yii::$app->session->setFlash('success', 'Спасибо за вопрос! Мы ответим Вам в ближайшее время!');
             } else {
                 Yii::$app->session->setFlash('error', 'There was an error sending your message.');
             }
--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -2,6 +2,7 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
+
 /* @var $model \frontend\models\ContactForm */
 
 use yii\helpers\Html;
@@ -16,51 +17,60 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1 style="padding-left:20px;"><?= Html::encode($this->title) ?></h1>
 
     <h4>
-        <center>Если у вас есть деловые вопросы или другие вопросы, пожалуйста, заполните следующую форму, чтобы связаться с нами. Спасибо.</center>
+        <center>Если у вас есть деловые вопросы или другие вопросы, пожалуйста, заполните следующую форму, чтобы
+            связаться с нами. Спасибо.
+        </center>
     </h4>
-        <div class="row">
-            <div class="col-md-2">
-            </div>
-            <div class="col-md-4">
-                <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-
-                    <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-
-                    <?= $form->field($model, 'email') ?>
-
-                    <?= $form->field($model, 'subject') ?>
-
-                    <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-
-                    <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                    ]) ?>
-
-                    <div class="form-group">
-                        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                    </div>
-
-                <?php ActiveForm::end(); ?>
-            </div>
-            <div class="col-md-6">
-                <p> Контакты:</p>
-                <p>+380666034768 (viber)- ст. менеджер Николай</p>
-                <p>+380677990756- ст. менеджер Николай</p>
-                <p>+380504788871- менеджер Андрей</p>
-                <p>+380953006640- сверка</p>
-                <p>imail: Lionauto.in.ua@gmail.com</p>
-                <p>icq: 699182412</p>
-                <br>
-                <p>График работы:</p>
-                <p>Пн: 9:00- 16:00</p>
-                <p>Вт:  9:00- 16:00</p>
-                <p>Ср: 9:00- 16:00<p>
-                <p>Чт: 9:00- 16:00</p>
-                <p>Пн: 9:00- 16:00</p>
-                <p>Сб: 9:00- 14:00</p>
-                <p>Вс: НЕ РАБОЧИЙ ДЕНЬ</p>
-            </div>
+    <div class="row">
+        <div class="col-md-2">
         </div>
+        <div class="col-md-4">
+            <?php $form = ActiveForm::begin(['enableClientValidation' => true, 'id' => 'contact-form']); ?>
+
+            <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+
+            <?= $form->field($model, 'email') ?>
+
+            <?= $form->field($model, 'phone')->widget(\yii\widgets\MaskedInput::className(), [
+                'mask' => '+38 (099) 999 99 99',
+                'clientOptions' => [
+                    'removeMaskOnSubmit' => true,
+                ]
+            ])->textInput();
+            ?>
+
+            <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
+
+            <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+            ]) ?>
+
+            <div class="form-group">
+                <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+        </div>
+        <div class="col-md-6">
+            <p> Контакты:</p>
+            <p>+380666034768 (viber)- ст. менеджер Николай</p>
+            <p>+380677990756- ст. менеджер Николай</p>
+            <p>+380504788871- менеджер Андрей</p>
+            <p>+380953006640- сверка</p>
+            <p>imail: Lionauto.in.ua@gmail.com</p>
+            <p>icq: 699182412</p>
+            <br>
+            <p>График работы:</p>
+            <p>Пн: 9:00- 16:00</p>
+            <p>Вт: 9:00- 16:00</p>
+            <p>Ср: 9:00- 16:00
+            <p>
+            <p>Чт: 9:00- 16:00</p>
+            <p>Пн: 9:00- 16:00</p>
+            <p>Сб: 9:00- 14:00</p>
+            <p>Вс: НЕ РАБОЧИЙ ДЕНЬ</p>
+        </div>
+    </div>
 
 
 </div>
