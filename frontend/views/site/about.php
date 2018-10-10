0dff395d437b0dc9434ad21a9ba4f27d53dd266b
Nikita Ketov <nketov@bigmir.net>
2018-10-10 11:56:14 +0300

    10102018

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -10,17 +10,17 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <br>
 <br><br><br><br><br><br>
-<section class="b-error s-shadow">
+<section class="b-error">
     <div class="container">
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
         <ul >
-            <li><a href="#" style="float:left">1. Прайс товаров (EXEL)</a></li>
-            <li><a href="#" style="float:left">2. Договор (WORD)</a></li>
-            <li><a href="#" style="float:left">3. Акт рекламации (WORD)</a></li>
-            <li><a href="#" style="float:left">4. Накладная возврата товара (EXEL)</a></li>
-            <li><a href="#" style="float:left">5. Заявление на рекламацию (WORD)</a></li>
-            <li><a href="#" style="float:left">6. Бланк (наряд заказ для СТО) (WORD)</a></li>
+            <li><a href="http://lion-auto.com.ua/documents/la810.xlsx" style="text-allign:left">1. Прайс товаров (EXEL)</a></li>
+            <li><a href="http://lion-auto.com.ua/documents/1DOGOVOR_POSTAVKI_BLANK_KOZAK.doc" style="text-allign:left">2. Договор (WORD)</a></li>
+            <li><a href="http://lion-auto.com.ua/documents/Akt_Reklamacii_LA.doc" style="text-allign:left">3. Акт рекламации (WORD)</a></li>
+            <li><a href="http://lion-auto.com.ua/documents/Vozvratnaja_Nakladnaja_LA.docx" style="text-allign:left">4. Накладная возврата товара (EXEL)</a></li>
+            <li><a href="http://lion-auto.com.ua/documents/Zajavlenie_na_reklamaciu_la.doc" style="text-allign:left">5. Заявление на рекламацию (WORD)</a></li>
+            <li><a href="http://lion-auto.com.ua/documents/nariad_zakz_la.xlsx" style="text-allign:left">6. Бланк (наряд заказ для СТО) (WORD)</a></li>
         </ul>
     
 
